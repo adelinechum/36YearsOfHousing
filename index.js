8c6425b446b1f7fa9fafc8b4873143611a0756bf
adelinechum <ac4626@columbia.edu>
2020-12-14 22:45:28 -0500

    pages + expanding

--- index.js
+++ index.js
@@ -6,27 +6,29 @@ var svg = d3.select("body").append("svg")
 * Global Size Variables
 */
 
-// Columns for YEAR
-var colWidth = 35;
-var colHeight = 150;
+var colWidth = 25;
+var colHeight = 300;
 var colXMargin = 1;
-var expansionFactor = 5;
+var expansionFactor = 3;
+var panelPerCol = 15 //number of panels we want per column
+var panelHeightMargin = 5;
+var panelHeight = (colHeight - panelHeightMargin * panelPerCol) / panelPerCol * 1.5;
+var panelWidth = colWidth;
 var centerX = window.innerWidth / 2;
-var centerY = window.innerHeight / 2 - colHeight / 2;
+var centerY = window.innerHeight / 2 - colHeight / 2; //location of grid matrix visualization
 
-// Super Class
 class Rectangle{
 
   static count = 0;
 
   constructor(){
-    this.form = svg.append("rect")
+    this.form = null;
     this.id = Rectangle.count;
     Rectangle.count++;
   }
 
   setColor(color){
-    this.form.attr("fill", color);
+    this.form.style("fill", color);
     return this;
   }
 
@@ -59,8 +61,11 @@ class Rectangle{
     return this;
   }
 
+
   }
 
+// TODO: make column height adjust to panels
+// TODO: center adjust columns instead of top aligned
 
 class Column extends Rectangle{
 
@@ -70,41 +75,66 @@ class Column extends Rectangle{
   // add a new column:
   // 1. move center to left
   // 2. update position of all columns
-  static addCol(){
+  static addCol(year){
     if (Column.cols.length) {
       Column.cols.forEach(col => {
-        col.setX(col.getX() - (colWidth + colXMargin) / 2)
+        // here shift all columns over to left to keep centered
+        col.setX(col.getX() - (colWidth + colXMargin) / 2);
+        col.updateTextPosition()
+        col.updatePanelsPositions();
       });
     }
 
-    Column.cols.push(new Column(centerX + (colWidth + colXMargin) * Column.cols.length / 2));
+    var res = new Column(centerX + (colWidth + colXMargin) * Column.cols.length / 2, year);
+    Column.cols.push(res);
+    return res;
   }
 
-
-  constructor(x){
+  constructor(x, year){
     super()
 
+    this.form = svg.append("rect")
+
+    this.panelNum = 0;
+
+    this.panels = [];
+
+    // initialize col position
     this.setX(x)
       .setY(centerY)
       .setWidth(colWidth)
       .setHeight(colHeight)
-      .setColor("#5a5a5a")
+      .setColor("#333")
 
-// Set transform / expand scale and unscale
+    // add text
+    this.text = svg.append("text")
+      .text(year)
+      .attr("x", this.getX())
+      .attr("y", this.getY())
+      .style("font-size", 8)
+
+ // TODO: fix mouseon when hover over panels
+
     this.form
-      .on("mouseover",
+      .on("mouseenter",
           () => {
-            this.transform(5);
+            this.transform(expansionFactor);
           }
       )
-      .on("mouseout",
+      .on("mouseleave",
           () => {
-            this.transform(1 / 5)
+            this.transform(1 / expansionFactor)
         }
       )
   }
 
-// Expanding Columns when hover
+  updatePanelsPositions(){
+    this.panels.forEach(panel => {
+      panel.updatePosition();
+    });
+
+  }
+
   transform(r){
     var x = this.getX();
     var y = this.getY();
@@ -117,10 +147,24 @@ class Column extends Rectangle{
     this.setWidth(this.getWidth() * r);
     this.setHeight(this.getHeight() * r);
 
+    // TODO: 'encapsulate' some in the panel class
+
+    var panelR = r
+
+    if(r < 1){
+      panelR = 1
+    }
+
+    this.panels.forEach(panel => {
+      panel.setWidth(panelWidth * panelR)
+      panel.setHeight(panelHeight * panelR)
+      panel.setY(this.getY() + panelHeightMargin * panelR + (panel.pos + 1) + panelHeight * (panel.pos) * panelR)
+      panel.setX(this.getX())
+    });
+
+
 
     // TODO: need a better way of differntiating on and off...
-    // Hacky way of moving columns to each side
-
     Column.cols.forEach(col => {
       if (col.id < this.id) {
         if (r > 1) {
@@ -138,14 +182,110 @@ class Column extends Rectangle{
           col.setX(col.getX() - colWidth * expansionFactor / 2)
         }
       }
+      if (col.id != this.id) {
+        col.updatePanelsPositions()
+      }
+      col.updateTextPosition()
     });
 
 
     }
 
+    updateTextPosition(){
+      this.text.attr("x", this.getX())
+        .attr("y", this.getY())
+    }
+
+    // add a new planel to the column
+    addPanel(img){
+      var panel = new Panel(this, img)
+      this.panels.push(panel);
+      this.panelNum ++;
+    }
+
 }
 
-// Here set number of columns
-for (var i = 0; i < 36; i++) {
-  Column.addCol()
+class Panel extends Rectangle{
+
+  constructor(col, img){
+
+    super();
+    this.col = col;
+    this.form = svg.append("image")
+      .attr("href", img);
+
+    // position of panel in col. 0 being highest
+    this.pos = col.panelNum
+
+    this.setX(col.getX())
+      .setY(col.getY() + panelHeightMargin * (this.pos + 1) + panelHeight * (this.pos))
+      .setWidth(panelWidth)
+      .setHeight(panelHeight);
+  }
+
+  updatePosition(){
+    this.setX(this.col.getX())
+      .setY(this.col.getY() + panelHeightMargin * (this.pos + 1) + panelHeight * (this.pos))
+  }
+
+  //j
+
+}
+
+d3.dsv(",", "./MasterData.csv", function(d) {
+  return {
+    year: d.Year, // convert "Year" column to Date
+    img: d.DocLink
+  };
+}).then(function(data) {
+
+  // filter out any empty images
+  data = data.filter(function (e) {
+    return e.img != "";
+  })
+
+  var years = uniq(data.map(function (e) {
+                return e.year
+              }))
+              .map(function (f) {
+                return {year: f, images: [] }
+              });
+
+      years.forEach((g) => {
+        data.forEach((h) => {
+          if (g.year == h.year) {
+            g.images.push(h.img)
+          }
+        });
+      });
+
+  years.forEach((e) => {
+      var col = Column.addCol(e.year);
+      e.images.forEach((img) => {
+        col.addPanel(img);
+      });
+  });
+
+});
+
+
+//
+// var rect = svg.append("rect")
+// .attr("width", 50)
+// .attr("height", 50)
+// .style("fill", "blue")
+//
+// var image = svg.append("image")
+//   .attr("width", 50)
+//   .attr("height", 50)
+//   .attr("href", "./pages/GSAPP_Exhibition-HousingStudios1974-2014_CompleteBooklet_010.jpg")
+
+
+
+// to return an array of unique values
+function uniq(a) {
+    var seen = {};
+    return a.filter(function(item) {
+        return seen.hasOwnProperty(item) ? false : (seen[item] = true);
+    });
 }
